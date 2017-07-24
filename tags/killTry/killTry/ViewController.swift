861eef53a5ecbac6aa0c543dced11dcb7517a956
skyfuckerzhao <1292554889@qq.com>
2017-07-24 09:42:28 +0800

    Site updated: 2017-07-24 09:42:25

---
+++ tags/killTry/killTry/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  killTry
+//
+//  Created by 郑嘉 on 2017/7/18.
+//  Copyright © 2017年 郑嘉. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
